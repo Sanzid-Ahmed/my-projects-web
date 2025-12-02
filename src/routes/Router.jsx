aafd855d8568530962849bceeea72b604e6cb883
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-12-02 19:32:43 +0600

    Initial commit of new React project

---
+++ src/routes/Router.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World!</h1>
+  }
+]);
