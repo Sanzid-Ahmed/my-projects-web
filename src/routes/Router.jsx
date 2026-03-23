1c644cf46669d115fc24dd2eda15093b370adfc4
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-03-23 20:03:46 +0600

    added jango initially

--- src/routes/Router.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router';
-
-export const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1>Hello World!</h1>
-  }
-]);
